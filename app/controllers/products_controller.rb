aeb4a48114ac0c45d101a5a97ae7e9dd33a8d92d
maryfranks <maryfranks@gmail.com>
2017-07-25 14:58:03 -0400

    generates products controller and empty methods

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,24 @@
+class ProductsController < ApplicationController
+
+  def new
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def updated
+  end
+
+  def destroy
+  end
+
+end
