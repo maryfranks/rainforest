94d575b75f1ed910116a9c30001d1089ba93872c
maryfranks <maryfranks@gmail.com>
2017-07-25 16:41:47 -0400

    adds data validation for product name, description, and price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price_in_cents, numericality: { only_integer: true }
+
   def price_in_dollars
     price_in_cents / 100
   end
