41ca60ac4f53e1797cb7a6e0cb956572301ec340
Martine Gagne <martine.e.gagne@gmail.com>
2017-07-25 16:15:01 -0400

    updates index view to show products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,9 @@ class ProductsController < ApplicationController
   end
 
   def index
+    @products = Product.all
+
+    # render 'index'
   end
 
   def create
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+
+  def price_in_dollars
+    price_in_cents / 100
+  end
+
 end
