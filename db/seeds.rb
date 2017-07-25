85c995b1889fe634828ad5d2a176dc86b25a897d
Martine Gagne <martine.e.gagne@gmail.com>
2017-07-25 15:13:29 -0400

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(
+  name: "toothbrush",
+  description: "medium, green handle",
+  price_in_cents: 200
+)
+
+Product.create(
+  name: "hairbrush",
+  description: "round brush for blowdrying",
+  price_in_cents: 500
+)
+
+Product.create(
+  name: "shampoo",
+  description: "best for dandruff",
+  price_in_cents: 800
+)
+
+Product.create(
+  name: "conditioner",
+  description: "best for dandruff",
+  price_in_cents: 900
+)
+
+Product.create(
+  name: "toothpaste",
+  description: "cavity fighting action",
+  price_in_cents: 300
+)
