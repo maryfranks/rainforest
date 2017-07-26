a4963418ee3d3c231ea007e62790aa7519bafb25
maryfranks <maryfranks@gmail.com>
2017-07-25 20:38:41 -0400

    attempts edit feature for products, minor bug: won't update prices

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,13 +26,30 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
-  def updated
+  def update
+
+    @product = Product.find(params[:id])
+    # @product.save(product_params)
+
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :edit
+    end
+
   end
 
   def destroy
   end
+
+  private
 
   def product_params
     params.require(:product).permit(:name, :description, :price_in_cents)
