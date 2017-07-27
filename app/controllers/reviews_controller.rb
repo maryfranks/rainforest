658d07e9e181ad9bb3f5e0d82729c12b46d9cdc2
maryfranks <maryfranks@gmail.com>
2017-07-27 17:43:32 -0400

    attempts to add edit review but internal server error

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,7 @@ class ReviewsController < ApplicationController
     @reviews = @product.reviews
 
     @review.comment = params[:review][:comment]
-    @review.product_id = product_id 
+    @review.product_id = product_id
 
     if @review.save
       redirect_to product_path(@product.id)
@@ -16,5 +16,21 @@ class ReviewsController < ApplicationController
     end
 
     flash[:notice] = "You have successfully created a new review!"
+  end
+
+  def update # INTERNAL SERVER ERROR LINE 23, 24, ALL OF THEM
+    @review = find(params[:id]
+    # product_id = params[:product_id]
+    @product = Product.find(product_id)
+    @reviews = @product.reviews
+
+    @review.comment = params[:review][:comment]
+    # @review.product_id = product_id
+
+    if @review.save
+      redirect_to product_path(@product.id)
+    else
+      render 'products/show'
+    end
   end
 end
