aa671de46384f0ce898ad5e087ed2908485c8512
Martine Gagne <martine.e.gagne@gmail.com>
2017-07-27 16:49:03 -0400

    creates partial for new review, renders partial on show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,9 +22,11 @@ class ProductsController < ApplicationController
 
   end
 
-  def show 
+  def show
     @product = Product.find(params[:id])
     @reviews = Review.where(product_id: @product.id)
+
+    @review = Review.new
   end
 
   def edit
