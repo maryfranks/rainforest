f42e0fd267b33826ccb3412090f85cabefa0f1ba
maryfranks <maryfranks@gmail.com>
2017-07-27 17:20:01 -0400

    adds function to submit new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new
+    product_id = params[:product_id]
+    @product = Product.find(product_id)
+    @reviews = @product.reviews
+
+    @review.comment = params[:review][:comment]
+    @review.product_id = product_id 
+
+    if @review.save
+      redirect_to product_path(@product.id)
+    else
+      render 'products/show'
+    end
+
+    flash[:notice] = "You have successfully created a new review!"
+  end
 end
