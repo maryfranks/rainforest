6d7e8b6f4e8ee59a65b8982e7b6e738fe87b8ba4
maryfranks <maryfranks@gmail.com>
2017-08-04 14:34:55 -0400

    fixes edit review feature

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,11 @@
 class ReviewsController < ApplicationController
 
+  def edit
+    @review = Review.find(params[:id])
+    product_id = params[:product_id]
+    @product = Product.find(product_id)
+  end
+
   def create
     @review = Review.new
     product_id = params[:product_id]
@@ -18,14 +24,15 @@ class ReviewsController < ApplicationController
     flash[:notice] = "You have successfully created a new review!"
   end
 
-  def update # INTERNAL SERVER ERROR LINE 23, 24, ALL OF THEM
-    @review = find(params[:id]
-    # product_id = params[:product_id]
+  def update
+    # byebug
+    @review = Review.find(params[:id])
+    product_id = params[:product_id]
     @product = Product.find(product_id)
     @reviews = @product.reviews
 
     @review.comment = params[:review][:comment]
-    # @review.product_id = product_id
+    @review.product_id = product_id
 
     if @review.save
       redirect_to product_path(@product.id)
